e3b946da1dd7fb29501464855216806d66cd1a2c
yin2002 <kimngan200173@gmail.com>
2021-11-21 01:09:29 +0700

    night

--- Areas/Admin/Controllers/DashboardController.cs
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -12,7 +12,7 @@ using comestic_csharp.Models;
 
 namespace comestic_csharp.Areas.Admin.Controllers
 {
-    [Authorize(Roles ="admin")]
+    [Authorize(Roles = "Admin")]
     [Area("Admin")]
     [Route("Admin/Dashboard")]
     public class DashboardController : Controller
