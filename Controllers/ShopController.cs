c576b4ea3fe881c7ead564ef4731b84318b62f33
yin2002 <kimngan200173@gmail.com>
2021-11-28 14:37:15 +0700

    2:36PM 28.11

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -43,5 +43,10 @@ namespace comestic_csharp.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
