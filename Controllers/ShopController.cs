1c80c782ec8fadcf5024c3e15bf66a70eac4a037
Le Thi Ai Nhi <19521963@gm.uit.edu.vn>
2021-12-21 22:22:15 +0700

    tong quat

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -9,6 +9,7 @@ using comestic_csharp.Models;
 using Microsoft.AspNetCore.Authorization;
 using comestic_csharp.Areas.Identity.Data;
 using PagedList.Core;
+using Microsoft.AspNetCore.Http;
 
 namespace comestic_csharp.Controllers
 {
@@ -25,27 +26,310 @@ namespace comestic_csharp.Controllers
         public IActionResult Index(int? page)
         {
             var pageNumber = page == null || page <= 0 ? 1 : page.Value;
-            var pagesize = 6;
-            var products = _context.Products.OrderByDescending(x => x.Price);
+            var pagesize = 9;
+            var products = _context.Products;
             PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
             ViewBag.CurrentPage = pageNumber;
+
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
             return View(model);
         }
-        public IActionResult list(int? page)
+
+        public IActionResult List(int? page)
         {
             var pageNumber = page == null || page <= 0 ? 1 : page.Value;
-            var pagesize = 6;
-            var products = _context.Products.OrderByDescending(x => x.Price);
+            var pagesize = 10;
+            var products = _context.Products;
             PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
             ViewBag.CurrentPage = pageNumber;
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+
             return View(model);
         }
 
-        // public async Task<IActionResult> list()
-        // {
-        //     return View(await _context.Products.ToListAsync());
-        // }
+        public IActionResult Range(int? page){
 
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 9;
+            var products = _context.Products.Where(p => p.Price <= 200000 && p.Price >=0);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+
+         public IActionResult Range2(int? page){
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 9;
+            var products = _context.Products.Where(p => p.Price <= 500000 && p.Price >=200000);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+         public IActionResult Range3(int? page){
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 9;
+            var products = _context.Products.Where(p =>  p.Price >=500000);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+        public IActionResult OrderByPriceA(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 10;
+            var products = _context.Products.OrderBy(x => x.Price);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+
+        public IActionResult OrderByPriceD(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 10;
+            var products = _context.Products.OrderByDescending(x => x.Price);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+
+        public IActionResult OrderByName(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 10;
+            var products = _context.Products.OrderByDescending(x => x.Title);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+            
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+
+        public IActionResult SortBy(string sort){
+
+            var url = $"/Shop/";
+
+            if(sort == "asc"){
+
+                url = $"/Shop/OrderByPriceA";
+
+            }
+            if(sort == "des"){
+
+               url = $"/Shop/OrderByPriceD";
+
+            }
+
+            if(sort == "name"){
+                url = $"/Shop/OrderByName";
+            }
+
+            return Json( new { status = "success", redirectUrl = url});
+        }
+        
+        public IActionResult Show(string show){
+
+            var url = $"/Shop/Range";
+            if(show == "1"){
+
+                url = $"/Shop/Range";
+
+            }
+            if(show == "2"){
+
+               url = $"/Shop/Range2";
+
+            }
+
+            if(show == "3"){
+                url = $"/Shop/Range3";
+            }
+            return Json( new { status = "success", redirectUrl = url});
+        }
         public async Task<IActionResult> Details(ulong? id)
         {
             if (id == null)
@@ -78,6 +362,70 @@ namespace comestic_csharp.Controllers
             return View();
         }
 
+        public async Task<IActionResult> ListMakeUp(ulong? id, int? page) {
+            // var ShopDbContext = _context.Products.Where(p => p.CatId == id);
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pagesize = 9;
+            var products = _context.Products.Where(p => p.CatId == id);
+            PagedList<Product> model = new PagedList<Product>(products,pageNumber,pagesize);
+            ViewBag.CurrentPage = pageNumber;
+
+            ViewData["SortBy"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Order by Price Asc", Value = "asc"},
+                    new SelectListItem { Text = "Order by Price Des", Value = "des"},
+                    new SelectListItem { Text = "Name", Value = "name"},
+
+                }, "Value" , "Text"
+
+            );
+
+            ViewData["Show"] = new SelectList(
+
+                 new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Dưới 200000", Value = "1"},
+                    new SelectListItem { Text = "Từ 200000 - 500000", Value = "2"},
+                    new SelectListItem { Text = "Trên 500000", Value = "3"},
+
+                }, "Value" , "Text"
+
+            );
+            return View(model);
+        }
+
+        public IActionResult SkinCare()
+        {
+            return View();
+        }
+
+        public IActionResult BestSeller()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            var profile = _context.Users.First(p => p.UserName == HttpContext.Session.GetString("username"));
+            return View(profile);
+        }
+
+        public IActionResult Order()
+        {
+            var id = _context.Users.First(p => p.UserName == HttpContext.Session.GetString("username"));
+            var order = _context.Orders.Where(p => p.UserId == id.Id);
+            return View(order);
+        }
+
+        public IActionResult OrderDetails(ulong id)
+        {
+            var orderdetails = _context.Orderdetail.Where(p => p.OrderId == id);
+            var order = _context.Orders.First(p => p.Id == id);
+            ViewBag.Order = order;
+            return View(orderdetails);
+        }
 
     }
 }
