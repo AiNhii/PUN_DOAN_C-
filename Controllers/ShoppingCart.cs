073151bc2db06d9a77df7e7f6671b72181c01dbc
Le Thi Ai Nhi <19521963@gm.uit.edu.vn>
2022-01-11 20:34:47 +0700

    thanh toán

--- Controllers/ShoppingCart.cs
+++ Controllers/ShoppingCart.cs
@@ -11,19 +11,31 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Identity;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using BraintreeHttp;
+using PayPal.v1.Payments;
+using Microsoft.AspNetCore.Authorization;
 
 namespace comestic_csharp.Controllers
 {
     public class ShoppingCartController : Controller
     {
-       private readonly ShopDbContext _context;
+        private readonly ShopDbContext _context;
         private readonly UserManager<ShopUser> _userManager;
 
+        private readonly string _clientId;
+        private readonly string _secretKey;
 
-        public ShoppingCartController(ShopDbContext context,UserManager<ShopUser> userManager)
+        public double TyGiaUSD = 22500;
+
+
+
+        public ShoppingCartController(ShopDbContext context,UserManager<ShopUser> userManager, IConfiguration config)
         {
             _context = context;
             _userManager = userManager;
+            _clientId = config["PaypalSettings:ClientId"];
+            _secretKey = config["PaypalSettings:SecretKey"];
         }
 
         // [Route ("/cart", Name = "cart")]
@@ -219,7 +231,7 @@ namespace comestic_csharp.Controllers
 
             if (ModelState.IsValid)
             {
-                Order donhang = new Order();
+                var donhang = new Models.Order();
                 donhang.OrderNumber= "ORDER"+ RandomString();
                 if (User.Identity.IsAuthenticated){
                     donhang.UserId = _userManager.GetUserId(User);
@@ -244,6 +256,182 @@ namespace comestic_csharp.Controllers
                     donhang.PaymentStatus ="unpaid";
                 }
                 
+                donhang.Status ="new";
+
+                _context.Add(donhang);
+                _context.SaveChanges();
+
+                        foreach( var item in cart){
+                             Orderdetail _orderDetail = new Orderdetail();
+                             _orderDetail.OrderId = donhang.Id;
+                             _orderDetail.ProductId = item.Product.Id;
+                             _orderDetail.Quantity = item.Quantity;
+                             _orderDetail.Price = item.Product.Price;
+                             _orderDetail.Total =  item.Product.Price * item.Quantity;
+                             _context.Add(_orderDetail);
+                        }
+                         _context.SaveChanges();
+
+                HttpContext.Session.Remove("save");
+                HttpContext.Session.Remove("final");
+                HttpContext.Session.Remove("couponId");
+                HttpContext.Session.Remove("cart");
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> PaypalCheckout()
+        {
+            var environment = new PayPal.Core.SandboxEnvironment(_clientId, _secretKey);
+            var client = new PayPal.Core.PayPalHttpClient(environment);
+
+            List<CartItem> cart = GetCartItems();
+
+            decimal save =0;
+            double _total = 0;
+
+            if(HttpContext.Session.GetInt32("save") != null)
+                    save = (int)HttpContext.Session.GetInt32("save");
+
+            foreach( var item in cart){
+                var thanhtien = item.Quantity * item.Product.Price;
+				_total += (double)thanhtien;	
+            }
+
+            #region Create Paypal Order
+            var itemList = new ItemList()
+            {
+                Items = new List<PayPal.v1.Payments.Item>()
+            };
+
+            var total = Math.Round(_total / TyGiaUSD, 2);
+            foreach (var item in cart)
+            {
+                itemList.Items.Add(new PayPal.v1.Payments.Item()
+                {
+                    Name = item.Product.Title,
+                    Currency = "USD",
+                    Price = Math.Round((double)item.Product.Price / TyGiaUSD, 2).ToString(),
+                    Quantity = item.Quantity.ToString(),
+                    Sku = "sku",
+                    Tax = "0"
+                });
+            }
+            #endregion
+
+            var paypalOrderId = DateTime.Now.Ticks;
+            var hostname = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+            var payment = new Payment()
+            {
+                Intent = "sale",
+                Transactions = new List<Transaction>()
+                {
+                    new Transaction()
+                    {
+                        Amount = new Amount()
+                        {
+                            Total = (total-(double)save).ToString(),
+                            Currency = "USD",
+                            Details = new AmountDetails
+                            {
+                                Tax = "0",
+                                Shipping = "0",
+                                Subtotal = total.ToString()
+                            }
+                        },
+                        ItemList = itemList,
+                        Description = $"Invoice #{paypalOrderId}",
+                        InvoiceNumber = paypalOrderId.ToString()
+                    }
+                },
+                RedirectUrls = new RedirectUrls()
+                {
+                    CancelUrl = $"{hostname}/ShoppingCart/Checkout",
+                    ReturnUrl = $"{hostname}/ShoppingCart/CheckoutSuccess"
+                },
+                Payer = new Payer()
+                {
+                    PaymentMethod = "paypal"
+                }
+            };
+
+            PaymentCreateRequest request = new PaymentCreateRequest();
+            request.RequestBody(payment);
+
+            try
+            {
+                var response = await client.Execute(request);
+                var statusCode = response.StatusCode;
+                Payment result = response.Result<Payment>();
+
+                var links = result.Links.GetEnumerator();
+                string paypalRedirectUrl = null;
+                while (links.MoveNext())
+                {
+                    LinkDescriptionObject lnk = links.Current;
+                    if (lnk.Rel.ToLower().Trim().Equals("approval_url"))
+                    {
+
+
+                        //saving the payapalredirect URL to which user will be redirected for payment  
+                        paypalRedirectUrl = lnk.Href;
+                        
+                    }
+                }
+
+                return Redirect(paypalRedirectUrl);
+            }
+            catch (BraintreeHttp.HttpException httpException)
+            {
+                var statusCode = httpException.StatusCode;
+                var debugId = httpException.Headers.GetValues("PayPal-Debug-Id").FirstOrDefault();
+
+                //Process when Checkout with Paypal fails
+                return Redirect("/ShoppingCart/CheckoutFail");
+            }
+        }
+
+        public IActionResult CheckoutFail()
+        {
+            return RedirectToAction("Checkout","ShoppingCart");
+        }
+        
+        public IActionResult CheckoutSuccess()
+        {
+            List<CartItem> cart = GetCartItems();
+            decimal save =0;
+            decimal total = 0;
+
+            if(HttpContext.Session.GetInt32("save") != null)
+                    save = (int)HttpContext.Session.GetInt32("save");
+            
+
+            foreach( var item in cart){
+                var thanhtien = item.Quantity * item.Product.Price;
+				total += thanhtien;	
+            }
+
+            if (ModelState.IsValid)
+            {
+                var donhang = new Models.Order();
+                donhang.OrderNumber= "ORDER"+ RandomString();
+                if (User.Identity.IsAuthenticated){
+                    donhang.UserId = _userManager.GetUserId(User);
+                }
+                if(HttpContext.Session.GetInt32("couponId") != 0){
+                    donhang.CouponId = (ulong?)HttpContext.Session.GetInt32("couponId");
+                }
+                donhang.FirstName = "FirstName";
+                donhang.LastName =  "LastName";
+                donhang.Email =  "Email";
+                donhang.Address =  "Address";
+                donhang.Phone =  "Phone";
+                donhang.TotalAmount = total - save ;
+                donhang.SubTotal = total;
+                donhang.CreatedAt = DateTime.Now;
+                donhang.PaymentMethod ="paypal";
+                donhang.PaymentStatus ="paid";
                 donhang.Status ="new";
 
                 _context.Add(donhang);
